8c4ae595a453e663a5c35b2befe6477bd47ed8d5
Werner Punz <werner.punz@gmail.com>
2017-10-18 08:56:33 +0200

    testing infra now in place... next step will be to write tests for the java source parsers so that we do not have to rely on third party libs and  compiled classes anymore for the data parsing

--- codegen_plugin/src/test/java/BasicTest.java
+++ codegen_plugin/src/test/java/BasicTest.java
@@ -1,7 +1,38 @@
+import com.intellij.openapi.application.ex.PathManagerEx;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.impl.JavaPsiFacadeEx;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class BasicTest {
+import com.intellij.testFramework.IdeaTestCase;
+
+public class BasicTest extends LightCodeInsightFixtureTestCase {
+
+    @Override
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/java/probes";
+    }
+
+    @Test
+   public void testFirstFile() {
+
+        myFixture.configureByFiles("TestDto.java");
+        PsiJavaFile psiJavaFile = (PsiJavaFile) myFixture.getFile();
+
+        assertTrue(psiJavaFile != null);
+        assertTrue(psiJavaFile.getClasses()[0].getQualifiedName().contains("TestDto"));
+  }
 
 }
---
+++ codegen_plugin/src/test/java/probes/TestDto.java
@@ -0,0 +1,19 @@
+package probes;
+
+public class TestDto {
+
+    String hello = "";
+
+    public TestDto(String hello) {
+        this.hello = hello;
+    }
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
+
