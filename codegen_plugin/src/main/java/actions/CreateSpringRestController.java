d34e2c112d97134295d1314ce06921a55d1f5818
Werner Punz <werner.punz@gmail.com>
2018-01-31 21:25:59 +0100

    started to work on the spring part

--- codegen_plugin/src/main/java/actions/CreateSpringRestController.java
+++ codegen_plugin/src/main/java/actions/CreateSpringRestController.java
@@ -3,10 +3,43 @@ package actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.fest.util.Maps;
+import utils.IntellijFileContext;
+import utils.IntellijUtils;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class CreateSpringRestController extends AnAction implements DumbAware {
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent) {
+    public void actionPerformed(AnActionEvent event) {
+        final Project project = IntellijUtils.getProject(event);
+        VirtualFile folder = IntellijUtils.getFolderOrFile(event);
+        IntellijFileContext ctx = new IntellijFileContext(project, folder);
+
+
+        VirtualFile srcRoot = ProjectFileIndex.getInstance(project).getSourceRootForFile(folder);
+        if(srcRoot != null) {//we are in a source root
+            Path targetPath = Paths.get(ctx.getFolderPath());
+            Path srcRootPath = Paths.get(srcRoot.getPath());
+
+            String packageName = srcRootPath.relativize(targetPath).toString()
+                    .replaceAll("[/\\\\]+", ".")
+                    .replaceAll("^\\.(.*)\\.$", "$1" );
+
+            Map<String, String> attrs = Maps.newHashMap();
+            attrs.put("PACKAGE_NAME", packageName);
+
+
+
+        }
+        //dtermine the package
+
+        //event.getProject().getWorkspaceFile().is
 
     }
 }
--- codegen_plugin/src/main/java/gui/CreateRequestMapping.java
+++ codegen_plugin/src/main/java/gui/CreateRequestMapping.java
@@ -1,4 +1,43 @@
 package gui;
 
+import javax.swing.*;
+
 public class CreateRequestMapping {
+    private JTextField textField1;
+    private JRadioButton rbGet;
+    private JRadioButton rbPost;
+    private JRadioButton rbPut;
+    private JRadioButton rbDelete;
+    private JCheckBox rbTypeScript;
+    private JComboBox cbReturnType;
+    private JCheckBox cbList;
+
+
+    public JRadioButton getRbGet() {
+        return rbGet;
+    }
+
+    public JRadioButton getRbPost() {
+        return rbPost;
+    }
+
+    public JRadioButton getRbPut() {
+        return rbPut;
+    }
+
+    public JRadioButton getRbDelete() {
+        return rbDelete;
+    }
+
+    public JCheckBox getRbTypeScript() {
+        return rbTypeScript;
+    }
+
+    public JComboBox getCbReturnType() {
+        return cbReturnType;
+    }
+
+    public JCheckBox getCbList() {
+        return cbList;
+    }
 }
